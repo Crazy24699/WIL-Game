a9462a556b7022977d971a493e0957f0c0ac55e6
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-03-02 17:13:16 +0200

    Locked and unlocked the mouse

---
+++ WIL Game/Assets/PlayerAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public PlayerInput PlayerInputRef;
+    protected InputAction InputRef;
+
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayerInputRef=new PlayerInput();
+    }
+
+    public void OnEnable()
+    {
+        InputRef = PlayerInputRef.PlayerAttack.ClawSlash;
+        InputRef.Enable();
+        PlayerInputRef.PlayerAttack.ClawSlash.Enable();
+
+    }
+
+    public void OnDisable()
+    {
+        InputRef.Disable();
+        PlayerInputRef.PlayerAttack.Disable();
+    }
+
+    public void Attack(InputAction.CallbackContext InputCallBack)
+    {
+
+    }
+
+    public void ClawAttack()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WIL Game/Assets/PlayerInteraction.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public InputActionReference ActionInput;
+
+    public PlayerInput PlayerInputRef;
+    protected InputAction InputRef;
+
+    public bool MenuActive;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        MenuActive = false ;
+        PlayerInputRef = new PlayerInput();
+    }
+
+    public void OnEnable()
+    {
+        InputRef = PlayerInputRef.PlayerInteraction.ShowMenu;
+        InputRef.Enable();
+
+        PlayerInputRef.PlayerInteraction.ShowMenu.Enable();
+        PlayerInputRef.PlayerInteraction.ShowMenu.performed += ChangeMenuState;
+
+    }
+
+    public void OnDisable()
+    {
+        InputRef.Disable();
+        PlayerInputRef.PlayerInteraction.ShowMenu.Disable();
+    }
+
+    public void ChangeMenuState(InputAction.CallbackContext InputCallBack)
+    {
+        
+        switch (MenuActive)
+        {
+            default:
+            case false:
+                Cursor.lockState = CursorLockMode.None;
+                MenuActive = true ;
+                break;
+
+
+            case true:
+                Cursor.lockState = CursorLockMode.Locked;
+                MenuActive = false ;
+                break;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
