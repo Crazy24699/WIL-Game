bf5c7aa7b1d20572cbbc4ac147e772fa17eebc63
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-05-08 18:22:36 +0200

    Updated on how the pathfinding will happen

--- WIL Game/Assets/Scripts/Enemy Scripts/Spewer AI/Behaviour Tree Nodes/BTPatrol.cs
+++ WIL Game/Assets/Scripts/Enemy Scripts/Spewer AI/Behaviour Tree Nodes/BTPatrol.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BTPatrol : BTNodeBase
 {
 
-    /// <summary>
+    /// <summary>           This will only happen if the AI Needs to be improved. 
     /// To Patrol the area the enemy will be in, The enemy will have 4 spires that is only visible within the editor
     /// and it will need to choose a random co ordindate on the x and z plane within those spires. It can not choose
     /// a coordinate that is within 5 meters of the last one, a distance will be drawn from the current cord and the new
@@ -13,21 +13,20 @@ public class BTPatrol : BTNodeBase
     /// which will determind the number of alternate waypoints. And then from that the waypoints will be altered bu a few 
     /// unites either positively or negatively
     /// </summary>
+    /// 
+                    ///The way the AI will be done. 
+    ///The way the current Ai will choose a location will be randomly choosing between 4 spires on the map
+    ///The chosen spire will be removed as to not allow the enemy to possibly get the same one indefinetly
+    ///spires will have a value that will determine the next one, it is up to random chance but the further 
+    ///a spire is from the current one, the more likely it is to be chosen
 
     protected EnemyBase EnemyAIScript;
 
-    protected HashSet<Transform> WaypointTransforms = new HashSet<Transform>();
     public List<Transform> SpirePoints;
 
     protected bool ReachedEndOfPath = false;
 
     public BTPatrol(GameObject EnemyAIRef)
-    {
-
-    }
-
-
-    protected void GenerateNewPath()
     {
 
     }
