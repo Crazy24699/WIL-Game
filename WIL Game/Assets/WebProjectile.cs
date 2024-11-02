5584a6e74ded80e2ad605007a47b66db72e798c9
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-11-02 20:34:50 +0200

    Started on shooting on the webs and its projectiles

--- WIL Game/Assets/WebProjectile.cs
+++ WIL Game/Assets/WebProjectile.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class WebProjectile : ProjectileBase
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameObject Webbing;
+
+    private bool IsProjectile = true;
+    private bool StartupRan = false;
+
+    public void Startup()
     {
-        
-    }
+
+
+        StartupRan = true;
+    } 
 
     // Update is called once per frame
     void Update()
     {
+        if (!StartupRan) { return; }
         
     }
+
+
+    private void OnTriggerEnter(Collider Collision)
+    {
+        if (Collision.CompareTag("Player"))
+        {
+            if(IsProjectile)
+            {
+                Collision.transform.root.GetComponent<PlayerInteraction>().HandleHealth(-Damage);
+                Destroy(this.gameObject);
+                return;
+            }
+
+        }
+
+        if(Collision.CompareTag("Ground") && IsProjectile)
+        {
+            IsProjectile = false;
+        }
+    }
+
+    class AttackPhases
+    {
+        //public 
+    }
+
 }
