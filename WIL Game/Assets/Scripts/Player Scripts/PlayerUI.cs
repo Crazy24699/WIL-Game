4477095c0f4e928dfee2a2836cfa9cf08e065a2a
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-11-13 22:59:07 +0200

    Added a basic options UI but its unfinished

--- WIL Game/Assets/Scripts/Player Scripts/PlayerUI.cs
+++ WIL Game/Assets/Scripts/Player Scripts/PlayerUI.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerUI : MonoBehaviour
 {
 
+    public AudioMixer MasterMixer;
 
+    private void Start()
+    {
+        SetVolume(0);
+    }
 
+    public void SetVolume(float SetVolume)
+    {
+        MasterMixer.SetFloat("MasterVolumeParm", SetVolume);
+    }
+
+    public void SetFullscreen(bool IsFullscreen)
+    {
+        Screen.fullScreen = IsFullscreen;
+    }
 
 }
